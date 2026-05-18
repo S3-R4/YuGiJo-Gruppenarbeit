10eb5fe47b4c96f09e674f50fdb657c7e8263ae5
S3-R4 <ahmet58karsag@gmail.com>
2026-05-18 16:52:06 +0200

    feat(model): (WIP) Klasse Karte implementiert.

--- src/model/Karte.java
+++ src/model/Karte.java
@@ -1,3 +1,18 @@
-public class Karte {
+package model;
 
+public class Karte {
+    private Held held;
+    
+    public Karte(Held held){
+        this.setHeld(held);
+    }
+
+    // Die Karten-GUI muss noch gebaut werden.
+
+    public Held getHeld(){
+        return this.held;
+    }
+    public void setHeld(Held held){
+        this.held = held;
+    }
 }
