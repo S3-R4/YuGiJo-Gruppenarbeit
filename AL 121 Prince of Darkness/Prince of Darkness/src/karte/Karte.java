6f9f4f0a09ca059dea9b3eddb4e7c0c22071292a
KarsagAhmet <ahmet58karsag@gmail.com>
2026-05-19 10:52:34 +0200

    feat(model): Karte

---
+++ AL 121 Prince of Darkness/Prince of Darkness/src/karte/Karte.java
@@ -0,0 +1,66 @@
+package karte;
+
+import java.awt.BorderLayout;
+import javax.swing.*;
+
+public class Karte extends JPanel {
+
+    private Held held;
+
+    public Karte(Held held){
+        JPanel p = new JPanel(new BorderLayout(10, 10));
+
+        //KOPFZEILE
+        JPanel kopfzeilePnl = new JPanel(new BorderLayout());
+        p.add(kopfzeilePnl, BorderLayout.NORTH);
+
+        JLabel heldName = new JLabel(held.getName());
+        kopfzeilePnl.add(heldName, BorderLayout.WEST);
+
+        JLabel heldTyp = new JLabel(held.getTyp());
+        kopfzeilePnl.add(heldTyp, BorderLayout.EAST);
+        // ENDE KOPFZEILE
+       
+
+        //BILD:
+        JPanel bildPnl = new JPanel();
+        bildPnl.add(new JLabel(held.getBild()));
+        p.add(bildPnl, BorderLayout.CENTER);
+        //ENDE BILD
+
+
+        //INFOPANEL:
+        JPanel infoPnl = new JPanel(new BorderLayout());
+        //Beschreibung
+        JLabel beschreibung = new JLabel(held.getBeschreibung());
+        infoPnl.add(beschreibung, BorderLayout.NORTH);
+
+        //Stats
+        JPanel statsPnl = new JPanel(new BoxLayout(new JPanel(), BoxLayout.Y_AXIS));
+        JLabel lebenLbl = new JLabel("" + held.getMaxLeben());
+        statsPnl.add(lebenLbl);
+
+        JLabel ruestungLbl = new JLabel("" + held.getRuestung());
+        statsPnl.add(ruestungLbl);
+
+        JLabel magieresistenzLbl = new JLabel("" + held.getMagieresistenz());
+        statsPnl.add(magieresistenzLbl);
+
+        JLabel angriffLbl = new JLabel("" + held.getAngriffswert());
+        statsPnl.add(angriffLbl);
+        infoPnl.add(statsPnl, BorderLayout.CENTER);
+        //ENDE STATS
+        //ENDE INFOPANEL
+
+
+        p.add(infoPnl, BorderLayout.SOUTH);
+        // Ende Komponenten
+    }
+
+    public Held getHeld(){
+        return this.held;
+    }
+    public void setHeld(Held held){
+        this.held = held;
+    }
+}
