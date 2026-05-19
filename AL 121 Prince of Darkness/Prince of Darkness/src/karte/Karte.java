a55a3dd94d258333e6ec6de23bb511315a48fa26
KarsagAhmet <ahmet58karsag@gmail.com>
2026-05-19 11:29:27 +0200

    FERTIG

--- AL 121 Prince of Darkness/Prince of Darkness/src/karte/Karte.java
+++ AL 121 Prince of Darkness/Prince of Darkness/src/karte/Karte.java
@@ -1,6 +1,7 @@
 package karte;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import javax.swing.*;
 
 public class Karte extends JPanel {
@@ -10,45 +11,57 @@ public class Karte extends JPanel {
     public Karte(Held held){
         this.setHeld(held);
         this.setLayout(new BorderLayout(10, 10));
+        this.setBackground(Color.GRAY);
 
         //KOPFZEILE
-        JPanel kopfzeilePnl = new JPanel(new BorderLayout());
+        JPanel kopfzeilePnl = new JPanel(new BorderLayout(10, 10));
+        kopfzeilePnl.setBackground(Color.RED);
         this.add(kopfzeilePnl, BorderLayout.NORTH);
 
         JLabel heldName = new JLabel(held.getName());
+        heldName.setAlignmentX(JLabel.LEFT_ALIGNMENT);
         kopfzeilePnl.add(heldName, BorderLayout.WEST);
 
         JLabel heldTyp = new JLabel(held.getTyp());
+        heldTyp.setAlignmentX(JLabel.RIGHT_ALIGNMENT);
         kopfzeilePnl.add(heldTyp, BorderLayout.EAST);
         // ENDE KOPFZEILE
        
 
         //BILD:
         JPanel bildPnl = new JPanel();
+        bildPnl.setBackground(Color.RED);
         bildPnl.add(new JLabel(held.getBild()));
         this.add(bildPnl, BorderLayout.CENTER);
         //ENDE BILD
 
 
         //INFOPANEL:
-        JPanel infoPnl = new JPanel(new BorderLayout());
+        JPanel infoPnl = new JPanel(new BorderLayout(10, 10));
+        infoPnl.setBackground(Color.RED);
         //Beschreibung
+        JPanel beschreibungPnl = new JPanel();
+        beschreibungPnl.setBackground(Color.RED);
         JLabel beschreibung = new JLabel(held.getBeschreibung());
-        infoPnl.add(beschreibung, BorderLayout.NORTH);
+        beschreibungPnl.add(beschreibung);
+        infoPnl.add(beschreibungPnl, BorderLayout.NORTH);
+
+        infoPnl.add(beschreibungPnl, BorderLayout.NORTH);
 
         //Stats
         JPanel statsPnl = new JPanel();
+        statsPnl.setBackground(Color.RED);
         statsPnl.setLayout(new BoxLayout(statsPnl, BoxLayout.Y_AXIS));
-        JLabel lebenLbl = new JLabel("" + held.getMaxLeben());
+        JLabel lebenLbl = new JLabel("Leben: " + held.getMaxLeben());
         statsPnl.add(lebenLbl);
 
-        JLabel ruestungLbl = new JLabel("" + held.getRuestung());
+        JLabel ruestungLbl = new JLabel("Ruestung: " + held.getRuestung());
         statsPnl.add(ruestungLbl);
 
-        JLabel magieresistenzLbl = new JLabel("" + held.getMagieresistenz());
+        JLabel magieresistenzLbl = new JLabel("Magieresistenz: " + held.getMagieresistenz());
         statsPnl.add(magieresistenzLbl);
 
-        JLabel angriffLbl = new JLabel("" + held.getAngriffswert());
+        JLabel angriffLbl = new JLabel("Angriffswert: " + held.getAngriffswert());
         statsPnl.add(angriffLbl);
         infoPnl.add(statsPnl, BorderLayout.CENTER);
         //ENDE STATS
